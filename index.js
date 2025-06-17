2dd35dc7babe0fa54aea25e1b17999c785cb2ca5
Brandons-Hub <brandon9oak@gmail.com>
2025-06-17 22:30:08 +0000

    update

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-console.log('Hello World!');
+document.getElementById("greeting").textContent = "This there this is the example text!";
 
-let name = 'wowzers';
-console.log(name);
+document.getElementById("h1").textContent = "This is the text i changed";
 
-let number = 5 && 6;
-console.log(number);
+let items = document.getElementsByClassName("redclass");
+for ( let i = 0; i < items.length; i++)
+{
+    items[i].textContent = "text change";
+};
+
+const header = document.querySelector("h1");
+console.log(header);
+
+const red = document.querySelectorAll(".redclass");
+console.log(red);
+
+document.getElementById("click").addEventListener("click" ,function()
+{
+    alert("Clicked Button!");
+});
